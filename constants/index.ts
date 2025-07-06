bb38b6f52d51d690766edaa510d8c39e74bb8b01
Altono1 <altonobinna11@gmail.com>
2025-07-06 15:26:07 +0100

    navroutes update

--- constants/index.ts
+++ constants/index.ts
@@ -4,40 +4,12 @@ export const navbarLinks = [
     label: "Home",
   },
   {
-    route: "/properties",
-    label: "Properties",
-  },
-  {
-    route: "/lands",
-    label: "Lands",
-  },
-  {
-    route: "/airbnb",
-    label: "AirBnB",
-  },
-  {
     route: "/blog",
     label: "Blog",
   },
 ];
 
 export const adminNavbarLinks = [
-  {
-    route: "/dashboard/properties",
-    label: "Properties",
-  },
-  {
-    route: "/dashboard/lands",
-    label: "Lands",
-  },
-  {
-    route: "/dashboard/airbnbs",
-    label: "AirBnB",
-  },
-  {
-    route: "/dashboard/bookings",
-    label: "Bookings",
-  },
   {
     route: "/dashboard/users",
     label: "Users",
